052eb2b45519f1ec9c35cd4ac27c081b41508fda
Shreyash <hello@webdevcaptain.com>
2025-01-21 10:26:17 +0000

    feat(cli): Integrate yamson cli with core functionality
    Accept the input and output filenames from command line and do the conversion

--- yamson-cli/src/main.rs
+++ yamson-cli/src/main.rs
@@ -1,24 +1,63 @@
-use std::fs;
-use yamson::json_to_yaml;
+use std::{env, fs, process};
+
+use yamson::{self, json_to_yaml, yaml_to_json};
 
 fn main() {
-    // JSON to Yaml
-    let input_filename = "sample.json";
-    let output_filename = "output.yaml";
+    // Parse command line args
+    let args: Vec<String> = env::args().collect();
 
-    let json_content = fs::read_to_string(input_filename).expect("Failed to read JSON file");
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input_file> <output_file>", args[0]);
+        process::exit(1);
+    }
 
-    json_to_yaml(&json_content, output_filename).expect("Failed to convert JSON to YAML");
+    let output_file = &args[2];
+    // Extract the input file extension
+    let input_file = &args[1];
+    let input_file_ext = input_file.split('.').last();
 
-    println!("JSON to YAML conversion successful");
+    let acceptable_input_file_extensions = ["yaml", "json", "yml"];
 
-    // Yaml to JSON
-    let input_filename = "sample.yaml";
-    let output_filename = "output.json";
+    if input_file_ext.is_none()
+        || !acceptable_input_file_extensions.contains(&input_file_ext.unwrap())
+    {
+        eprintln!("Invalid input file: {}", input_file);
+        process::exit(1);
+    }
 
-    let yaml_content = fs::read_to_string(input_filename).expect("Failed to read YAML file");
+    let convert_from = input_file_ext.unwrap();
 
-    yamson::yaml_to_json(&yaml_content, output_filename).expect("Failed to convert YAML to JSON");
+    // Read input file content
+    let input_content = match fs::read_to_string(input_file) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Error reading input file : {}", err);
+            process::exit(1);
+        }
+    };
 
-    println!("YAML to JSON conversion successful");
+    // Perform conversion based on input file extension
+    match convert_from {
+        "json" => {
+            if let Err(err) = json_to_yaml(&input_content, output_file) {
+                eprintln!("Error converting to YAML: {}", err);
+                process::exit(1);
+            }
+        }
+        "yaml" | "yml" => {
+            if let Err(err) = yaml_to_json(&input_content, output_file) {
+                eprintln!("Error converting to JSON: {}", err);
+                process::exit(1);
+            }
+        }
+        _ => {
+            eprintln!(
+                "Invalid conversion type: '{}'. Use `json` or `yaml` ",
+                convert_from
+            );
+            process::exit(1);
+        }
+    }
+
+    println!("Done ✅");
 }
